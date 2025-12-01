b2ffdece1ed8eaea81877466f620f79637dd494e
Narendra S <narendra.s1232@gmail.com>
2025-12-01 07:01:21 +0530

    Update 0001_init.sql

--- backend/migrations/0001_init.sql
+++ backend/migrations/0001_init.sql
@@ -13,5 +13,4 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
